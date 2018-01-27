cf278f820c73321c0932cd1a6ac5a6d7f32ee9a4
HDVinnie <unit3d@protonmail.com>
2018-01-27 13:11:38 -0500

    (Update) Refactor PT.10
    - Create Poll Config File

---
+++ config/poll.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ * @license    https://choosealicense.com/licenses/gpl-3.0/  GNU General Public License v3.0
+ * @author     HDVinnie
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Max Poll Options
+    |--------------------------------------------------------------------------
+    |
+    | The maximum number of options a poll can be created with.
+    |
+    */
+
+    'max_options' => 10,
+
+];
